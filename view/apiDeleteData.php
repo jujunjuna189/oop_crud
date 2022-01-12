53b6ef30c25a27d0162a706d648b16413c33e2b0
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2022-01-12 11:11:57 +0700

    Initial commit

---
+++ view/apiDeleteData.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Content-Type:application/json;charset=UTF-8");
+header("Access-Control-Allow-Methods:POST,GET,PUT,DELETE");
+header("Access-Control-Max-Age:3600");
+header("Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With");
+// Get Database Connection
+include '../controller/SekolahController.php';
+$ctrl = new SekolahController();
+$request = $_SERVER['REQUEST_METHOD'];
+switch ($request) {
+    case 'POST':
+        $ctrl->hapusData();
+        break;
+    default:
+        http_response_code(404);
+        echo 'Request tidak diizinkan';
+}
